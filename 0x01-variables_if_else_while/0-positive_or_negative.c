c98030d12963c55fd95f9e717dd9ffdaa3426ff3
Ahazzia <emmanuelbyiringiro9@gmail>
2023-02-17 08:44:57 -0800

    error_6

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,9 @@
 */
 int main(void)
 {
-int a = 1103515245;  /* multiplier */
-int c = 12345;/* increment */
-int m = 2147483648;  /* modulus */
-int x = time(NULL);  /* seed */
 int n;
-x = (a * x + c) % m;  /* update seed */
-n = x % 100; /* generate a random integer between 0 and 99 inclusive */
-/* your code goes there */
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf("%d is positive ", n);
