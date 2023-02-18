52ea1ba64ca7d46f580c0b3410a02d15d20c81be
Ahazzia <emmanuelbyiringiro9@gmail>
2023-02-18 09:12:14 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - entry
+ * Return: (0) for success
+*/
+int main(void)
+{
+char lower;
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+putchar (lower);
+}
+putchar('\n');
+return (0);
+}
