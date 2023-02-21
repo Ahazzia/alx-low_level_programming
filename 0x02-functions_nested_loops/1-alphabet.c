9c62d8def35e7cc005bc6b61c28806d93123ae83
Ahazzia <emmanuelbyiringiro9@gmail>
2023-02-21 05:11:01 -0800

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry
+ * Return: 0 for seccess
+ * Description: lowercase letters
+*/
+int main(void)
+{
+int a, b;
+if (a < 'a' && b >= 'z')
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
+
