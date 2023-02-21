ad147e0f8fd9cbd9585ffebd835637f86a780471
Ahazzia <emmanuelbyiringiro9@gmail>
2023-02-21 02:38:00 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+ * main - entry point
+ * Return: 0 for success
+ *
+ * Description: printing _putchar followed by new line 
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
