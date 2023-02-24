ea4b6da1b563e9d3d8fa8bd4596135f4ac33585c
Ahazzia <emmanuelbyiringiro9@gmail>
2023-02-24 03:25:52 -0800

    task 3: print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print the numbers from 0 to 9
+ * Return: 0 fro success
+ */
+
+void print_numbers(void)
+{
+int a;
+
+if (a >= 0 && a <= 9)
+{
+_putchar(i + '0');
+_putchar('\n');
+}
+}
