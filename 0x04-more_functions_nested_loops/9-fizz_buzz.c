59d4a09a0a586f389fbc7be0062d72e7bfe492ff
Ahazzia <emmanuelbyiringiro9@gmail>
2023-02-24 08:14:21 -0800

    task 9: Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Entry Point
+ * main - prints numberrs and fizz or buzz over some conditions
+ *
  * Return: 0 for success
- */
+*/
 int main(void)
 {
 int i;
 
-for (i = 1; i < 101; i++)
+for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 {
-printf(" %d ", i);
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
 }
 }
+printf("\n");
 return (0);
 }
