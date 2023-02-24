f1a103db9f42fecafff5fb48d37da71fd1809c74
Ahazzia <emmanuelbyiringiro9@gmail>
2023-02-24 08:11:17 -0800

    task 9: Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,21 @@ int i;
 
 for (i = 1; i < 101; i++)
 {
-if (i%3 == 0 && i%5 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz ");
 }
-else if (i%3 == 0)
+else if (i % 3 == 0)
 {
 printf("Fizz ");
 }
-else if(i%5 ==0)
+else if (i % 5 == 0)
 {
 printf("Buzz ");
 }
 else
 {
-printf("%d ", i);
+printf(" %d ", i);
 }
 }
 return (0);
