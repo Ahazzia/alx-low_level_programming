d59c8fe61fe1bc9e2050a684f0704cc7dd18067a
Ahazzia <emmanuelbyiringiro9@gmail>
2023-02-27 09:17:36 -0800

    My README.md and some function that assign a variable to a new value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 : the functio to reset the value of a variable
- * @n: a valuabe variable 
- * Return: some value for success
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * it points to to 98
+ * @n: my valuable
+ * Return: value for success excution
  */
 
 void reset_to_98(int *n)
